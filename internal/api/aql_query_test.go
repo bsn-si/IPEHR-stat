f2b842d8ad8efed44a45f9350a83d88552882803
Valeriy Tetevin <seal.matob@gmail.com>
2023-02-08 18:10:49 +0300

    IPEHR-214 fix linter error and golangci-ling config

--- internal/api/aql_query_test.go
+++ internal/api/aql_query_test.go
@@ -80,8 +80,6 @@ func TestAQLQueryAPI_QueryHandler(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 
